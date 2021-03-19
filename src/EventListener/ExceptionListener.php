e4f8bc979f59d91555687e9948f038a974d3cdad
Quentin Gans <contact@quentingans.fr>
2021-03-19 15:28:27 +0100

    Fix ExceptionListener

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -5,11 +5,16 @@ namespace App\EventListener;
 use App\Controller\BaseController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ExceptionListener
 {
     public function onKernelException(ExceptionEvent $event): void
     {
+        if (!$event->getThrowable() instanceof NotFoundHttpException) {
+            return;
+        }
+
         // Sends the modified response object to the event
         $event->setResponse(new JsonResponse([
             'status' => BaseController::ERROR,
