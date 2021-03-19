266cdce64c2cb16a11786566bee3bcd3659a8eed
Quentin Gans <contact@quentingans.fr>
2021-03-19 15:51:07 +0100

    Add admin theme

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,15 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Controller\BaseController;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin', name: 'admin.')]
 class DashboardController extends BaseController
 {
+    public function __construct(
+        private ProductRepository $productRepository
+    ) {
+    }
+
     #[Route('', name: 'index')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig');
+        return $this->render('admin/index.html.twig', [
+            'nbrProducts' => $this->productRepository->count(['parentId' => null])
+        ]);
     }
 }
