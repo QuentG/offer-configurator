1782cb084da757b875bb4b085c80bdb60b83ed27
Quentin Gans <contact@quentingans.fr>
2021-03-19 11:37:11 +0100

    Add base admin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,22 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private const USER_PASSWORD = 'password';
+
     private Generator $faker;
 
-    public function __construct()
+    public function __construct(
+        private UserPasswordEncoderInterface $userPasswordEncoder
+    )
     {
         $this->faker = Factory::create('fr_FR');
     }
 
     public function load(ObjectManager $manager): void
     {
+        $user = new User();
+        $user->setEmail('admin@gmail.com')
+            ->setPassword($this->userPasswordEncoder->encodePassword($user, self::USER_PASSWORD));
+
+        $manager->persist($user);
+
         $manager->flush();
     }
 }
