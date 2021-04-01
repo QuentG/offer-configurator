a2130b3e9ce3874cf8df83f11effc0725ac0ccc7
Quentin Gans <contact@quentingans.fr>
2021-04-01 15:22:38 +0200

    Add eventlistener test

--- tests/EventListener/ExceptionListenerTest.php
+++ tests/EventListener/ExceptionListenerTest.php
@@ -1,10 +1,17 @@
 <?php
 
-
 namespace App\Tests\EventListener;
 
+use App\Controller\BaseController;
+use App\Tests\ApiTestCase;
+use Symfony\Component\HttpFoundation\Request;
 
-class ExceptionListenerTest
+class ExceptionListenerTest extends ApiTestCase
 {
+    public function testRouteNotFound(): void
+    {
+        $response = $this->jsonRequest(Request::METHOD_GET, '/api/toto');
 
+        $this->assertJsonEqualsToJson($response, BaseController::ERROR, 'route_not_found');
+    }
 }
