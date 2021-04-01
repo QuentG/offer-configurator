af4d0d2e98a658719707cd6cc980adad4ef1d7a7
Quentin Gans <contact@quentingans.fr>
2021-04-01 15:22:01 +0200

    Update test product api

--- tests/Controller/Api/ProductControllerTest.php
+++ tests/Controller/Api/ProductControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Controller\Api;
 
+use App\Controller\BaseController;
+use App\Entity\Product;
 use App\Tests\ApiTestCase;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,6 +24,22 @@ class ProductControllerTest extends ApiTestCase
 
         $this->client->request(Request::METHOD_GET, '/api/products/' . $product->getId());
         // Check response code
+        self::assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    public function testProductChildrens(): void
+    {
+        $productRepository = static::$container->get(ProductRepository::class);
+        $mainProduct = $productRepository->findOneBy([
+            'parentId' => null,
+            'type' => Product::PARENT_TYPE
+        ]);
+
+        $this->client->request(
+            Request::METHOD_GET,
+            sprintf('/api/products/%s/childrens/%s', $mainProduct->getId(), json_encode(['M']))
+        );
+
         self::assertResponseStatusCodeSame(Response::HTTP_OK);
     }
 }
